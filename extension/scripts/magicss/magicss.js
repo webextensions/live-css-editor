d9c8820393fd537d417efb241ad50aacd387b4a7
Priyank Parashar <priyank@one.com>
2017-04-16 17:15:46 +0400

    Don't let mouse scroll on CodeMirror hints pass on to the parent elements and bumped version to 4.1.2

--- extension/scripts/magicss/magicss.js
+++ extension/scripts/magicss/magicss.js
@@ -223,6 +223,36 @@
                 // do nothing
             },
             fnSuccess: function () {
+                // Don't let mouse scroll on CodeMirror hints pass on to the parent elements
+                // http://stackoverflow.com/questions/5802467/prevent-scrolling-of-parent-element/16324762#16324762
+                $(document).on('DOMMouseScroll mousewheel', '.CodeMirror-hints', function(ev) {
+                    var $this = $(this),
+                        scrollTop = this.scrollTop,
+                        scrollHeight = this.scrollHeight,
+                        height = $this.innerHeight(),
+                        delta = (ev.type == 'DOMMouseScroll' ?
+                            ev.originalEvent.detail * -40 :
+                            ev.originalEvent.wheelDelta),
+                        up = delta > 0;
+
+                    var prevent = function() {
+                        ev.stopPropagation();
+                        ev.preventDefault();
+                        ev.returnValue = false;
+                        return false;
+                    };
+
+                    if (!up && -delta > scrollHeight - height - scrollTop) {
+                        // Scrolling down, but this will take us past the bottom.
+                        $this.scrollTop(scrollHeight);
+                        return prevent();
+                    } else if (up && delta > scrollTop) {
+                        // Scrolling up, but this will take us past the top.
+                        $this.scrollTop(0);
+                        return prevent();
+                    }
+                });
+
                 var smc;
 
                 var id = 'MagiCSS-bookmarklet',
