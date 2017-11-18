39225b9668d1b5e01454b791ed711844085ed3b2
Priyank Parashar <priyank@one.com>
2017-11-18 15:21:27 +0400

    wip for live file editor

--- extension/scripts/magicss/editor/editor.js
+++ extension/scripts/magicss/editor/editor.js
@@ -966,6 +966,8 @@ var USER_PREFERENCE_AUTOCOMPLETE_SELECTORS = 'autocomplete-css-selectors',
         }
     }
     Editor.defaultPreferences = {
+        'magic-css-server-path': 'http://localhost:3777',  // TODO: Move this into Magic CSS code
+
         'language-mode': 'css',
         'use-css-linting': 'no',
         'disable-styles': 'no',
