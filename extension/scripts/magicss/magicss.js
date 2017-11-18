c280e1f7699c0ce0030a3a3c6f2bb925d83f4f53
Priyank Parashar <priyank@one.com>
2017-11-19 00:21:41 +0400

    wip for live file editor

--- extension/scripts/magicss/magicss.js
+++ extension/scripts/magicss/magicss.js
@@ -1230,11 +1230,16 @@ var USER_PREFERENCE_AUTOCOMPLETE_SELECTORS = 'autocomplete-css-selectors',
                     });
                 };
 
-                var getDataForFileToEdit = function (editor, cb) {
+                var getDataForFileToEdit = function (editor, options, cb) {
+                    options = options || {};
                     var needInputThroughUi = true;
-                    console.log('TODO: Set needInputThroughUi to true/false');
 
-                    if (needInputThroughUi) {
+                    var pathOfFileToEdit = editor.userPreference('file-to-edit');
+                    if (pathOfFileToEdit) {
+                        needInputThroughUi = false;
+                    }
+
+                    if (needInputThroughUi || options.showUi) {
                         showFileEditOptions(editor, function (filePath) {
                             // var fileSuggestions = window.fileSuggestions;
                             // var filePath = fileSuggestions.getValue()[0];
@@ -1266,7 +1271,15 @@ var USER_PREFERENCE_AUTOCOMPLETE_SELECTORS = 'autocomplete-css-selectors',
                             /* */
                         });
                     } else {
-                        console.log('TODO');
+                        loadFile(
+                            editor,
+                            {
+                                filePath: pathOfFileToEdit
+                            },
+                            function (file) {
+                                cb(file);
+                            }
+                        );
                     }
                 };
 
@@ -1281,7 +1294,7 @@ var USER_PREFERENCE_AUTOCOMPLETE_SELECTORS = 'autocomplete-css-selectors',
 
                 var setLanguageMode = function (languageMode, editor) {
                     if (languageMode === 'file') {
-                        getDataForFileToEdit(editor, function (file) {
+                        getDataForFileToEdit(editor, {}, function (file) {
                             editor.options.rememberText = false;
 
                             setLanguageModeClass(editor, 'magicss-selected-mode-file');
@@ -2027,8 +2040,19 @@ var USER_PREFERENCE_AUTOCOMPLETE_SELECTORS = 'autocomplete-css-selectors',
                         // });
                         /* */
 
-                        $footerItems.on('mousedown', function (evt) {
-                            evt.stopPropagation();
+                        // $footerItems.on('mousedown', function (evt) {
+                        //     evt.stopPropagation();
+                        // });
+
+                        $footerForFileMode.on('click', function () {
+                            getDataForFileToEdit(editor, {showUi: true} ,function (file) {
+                                $('.footer-for-file-mode .name-of-file-being-edited').html(htmlEscape(getFileNameFromPath(file.path)));
+                                utils.alertNote('Now editing file: ' + htmlEscape(file.path), 5000);
+                                editor
+                                    .setTextValue(file.contents)
+                                    .reInitTextComponent({pleaseIgnoreCursorActivity: true})
+                                    .focus();
+                            });
                         });
 
                         // Magic Suggest uses old jQuery code. Minor changes to fix that
