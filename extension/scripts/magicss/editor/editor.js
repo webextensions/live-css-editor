24a75d418012706cc2500f434257ca1d333efa75
Priyank Parashar <priyank@one.com>
2017-11-18 23:59:20 +0400

    wip for live file editor

--- extension/scripts/magicss/editor/editor.js
+++ extension/scripts/magicss/editor/editor.js
@@ -996,6 +996,7 @@ var USER_PREFERENCE_AUTOCOMPLETE_SELECTORS = 'autocomplete-css-selectors',
 
             var $that = $(thisOb.container);
             $that.addClass('mode-codeMirror');
+            return this;
         }
 
         setCursor(cursorPosition, options) {
@@ -1010,6 +1011,7 @@ var USER_PREFERENCE_AUTOCOMPLETE_SELECTORS = 'autocomplete-css-selectors',
             if (options.pleaseIgnoreCursorActivity) { this.pleaseIgnoreCursorActivity = true; }
             this.reInitCodeMirror();
             if (options.pleaseIgnoreCursorActivity) { this.pleaseIgnoreCursorActivity = false; }
+            return this;
         }
     }
     Editor.defaultPreferences = {
