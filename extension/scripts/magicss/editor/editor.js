802a42c877868d291af6ebd35eb8a2ad1cb84a78
Priyank Parashar <paras20xx@gmail.com>
2020-02-14 06:04:25 +0400

    Stop keyboard event propagation outside the code editor

--- extension/scripts/magicss/editor/editor.js
+++ extension/scripts/magicss/editor/editor.js
@@ -728,6 +728,15 @@ var USER_PREFERENCE_AUTOCOMPLETE_SELECTORS = 'autocomplete-css-selectors',
                     await thisOb.triggerEvent('delayedtextchange');
                 }, delay);
             });
+            cm.on('keydown', function(cm, evt) {
+                evt.stopPropagation();
+            });
+            cm.on('keypress', function(cm, evt) {
+                evt.stopPropagation();
+            });
+            cm.on('keyup', function(cm, evt) {
+                evt.stopPropagation();
+            });
 
             // http://stackoverflow.com/questions/4179708/how-to-detect-if-the-pressed-key-will-produce-a-character-inside-an-input-text/4180715#4180715
             var isCharacterKeyPress = function (evt) {
