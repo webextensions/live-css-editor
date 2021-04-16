07e57ecb0511ef87a14ef8f114a4a5387c1cc58e
Priyank Parashar <paras20xx@gmail.com>
2021-04-17 01:50:09 +0400

    Bumped version to 8.10.1

--- extension/scripts/appVersion.js
+++ extension/scripts/appVersion.js
@@ -1,7 +1,7 @@
 /* globals module */
 // Just a block
 {
-    const magicCssVersion = '8.10.0';
+    const magicCssVersion = '8.10.1';
     if (typeof window === 'undefined') {
         module.exports = {
             version: magicCssVersion
