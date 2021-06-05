04a6128820d58d28055bbc5e62e8691c59efd549
Priyank Parashar <paras20xx@gmail.com>
2021-06-05 11:53:30 +0400

    8.13.4

--- extension/scripts/appVersion.js
+++ extension/scripts/appVersion.js
@@ -1,6 +1,6 @@
 // Just a block
 {
-    const magicCssVersion = '8.13.3';
+    const magicCssVersion = '8.13.4';
     if (typeof window === 'undefined') {
         module.exports = {
             version: magicCssVersion
