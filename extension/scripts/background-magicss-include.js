448a5be1208635fd3ff5e52421798d93613d4a47
Priyank Parashar <paras20xx@gmail.com>
2022-11-20 12:46:33 +0400

    8.19.1

--- extension/scripts/background-magicss-include.js
+++ extension/scripts/background-magicss-include.js
@@ -27,7 +27,7 @@ var fallbackConfig = {
             }
         }
     },
-    "version": "8.19.0"
+    "version": "8.19.1"
 };
 var remoteConfig = JSON.parse(JSON.stringify(fallbackConfig));
 var instanceUuid = null;
