e7320bf102ec8a33f56aa05ffd19739b692bcc8c
Priyank Parashar <paras20xx@gmail.com>
2023-04-17 02:48:34 +0400

    8.21.3

--- extension/scripts/background-magicss-include.js
+++ extension/scripts/background-magicss-include.js
@@ -45,7 +45,7 @@ var fallbackConfig = {
             }
         }
     },
-    "version": "8.21.2"
+    "version": "8.21.3"
 };
 let remoteConfig = JSON.parse(JSON.stringify(fallbackConfig));
 myWin.remoteConfig = remoteConfig;
