007c1f7fb68bcc4495c88d306bde1c193c21ba52
Priyank Parashar <paras20xx@gmail.com>
2023-05-10 03:58:51 +0400

    Fixed the issue of the colorpicker not working

--- extension/scripts/3rdparty/codemirror/addons/colorpicker/colorview_customized.js
+++ extension/scripts/3rdparty/codemirror/addons/colorpicker/colorview_customized.js
@@ -1,9 +1,9 @@
 (function(mod) {
-    if (typeof exports == "object" && typeof module == "object") // CommonJS
-        mod(require("codemirror"));
-    else if (typeof define == "function" && define.amd) // AMD
-        define(["codemirror"], mod);
-    else // Plain browser env
+    // if (typeof exports == "object" && typeof module == "object") // CommonJS
+    //     mod(require("codemirror"));
+    // else if (typeof define == "function" && define.amd) // AMD
+    //     define(["codemirror"], mod);
+    // else // Plain browser env
         mod(CodeMirror);
 })(function(CodeMirror) {
     "use strict";
