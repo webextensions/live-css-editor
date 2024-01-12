baab5714cf17d56f21ccc71fcc5d6bda3cbcfe56
priyank <priyank@example.com>
2024-01-13 02:28:50 +0400

    8.22.1

--- extension/scripts/appVersion.js
+++ extension/scripts/appVersion.js
@@ -1,6 +1,6 @@
 // Just a block
 {
-    const magicCssVersion = '8.22.0';
+    const magicCssVersion = '8.22.1';
     if (typeof window === 'undefined') {
         module.exports = {
             version: magicCssVersion
--- extension/scripts/background-magicss-include.js
+++ extension/scripts/background-magicss-include.js
@@ -45,7 +45,7 @@ var fallbackConfig = {
             }
         }
     },
-    "version": "8.22.0"
+    "version": "8.22.1"
 };
 let remoteConfig = JSON.parse(JSON.stringify(fallbackConfig));
 myWin.remoteConfig = remoteConfig;
